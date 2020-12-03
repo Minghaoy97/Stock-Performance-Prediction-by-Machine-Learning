55fb7588ae3a66f695b909b63f8bcbb726572d09
Minghaoy97 <71526139+Minghaoy97@users.noreply.github.com>
2020-12-03 11:39:44 -0500

    code for combining technique and fundamental factors
    A comprehensive version of processing fundamental and technique factors data

--- EECS 545 project.py
+++ EECS 545 project.py
@@ -1,7 +1,44 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[78]:
+
+
+import xlrd  
+
+print('Please input the fundamental xlsx file name:') # For example, apple.xlsx
+fundamental_filename = input()
+print('Please input the price cvs file name:')# For example, AAPL.csv
+price_filename = input()
+
+
+# In[85]:
+
+
+import pandas as pd
+from collections import defaultdict
+from pandas.core.frame import DataFrame
+
+fund_file = xlrd.open_workbook(fundamental_filename)
+
+datasheet = fund_file.sheets()[0]
+select_data = []
+for i in range(datasheet.nrows):  
+    if '' not in datasheet.row_values(i):
+        select_data.append(datasheet.row_values(i))
+        
+for i in range(len(select_data)):
+    for j in range(len(select_data[i])):
+        if select_data[i][j] == '-':
+            select_data[i][j] = select_data[i][j - 1]
+
+fund_data = defaultdict(list)
+for i in select_data:
+    fund_data[i[0]] = i[1:]
+data = DataFrame(fund_data)
+
+
+# In[86]:
 
 
 import numpy as np
@@ -9,32 +46,13 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-# In[112]:
-
-
-df = pd.read_csv("D:/UMICH/EECS 545/project/AAPL.csv")
-# df.set_index("Date", inplace=True)
-
-
-# In[27]:
-
-
 def gen_simple_ma(df, ma = 5):
     df["MA" + str(ma)] = df["Close"].rolling(window=ma, center=False).mean()
     return df
 
-
-# In[53]:
-
-
 def gen_ewma(df, ma = 5):
     df["EMA" + str(ma)] = df["Close"].ewm(span = ma, min_periods = ma).mean()
     return df
-
-
-# In[66]:
-
 
 def gen_RSI(df, periods=5):
     array_list = df["Close"]
@@ -72,10 +90,6 @@ def gen_RSI(df, periods=5):
     df["RSI"+" "+str(periods)] = rsies
     return df
 
-
-# In[93]:
-
-
 def gen_ADO(data, trend_periods=5, open_col='Open', high_col='High', low_col='Low', close_col='Close', vol_col='Volume'):
     for index, row in data.iterrows():
         if row[high_col] != row[low_col]:
@@ -85,10 +99,6 @@ def gen_ADO(data, trend_periods=5, open_col='Open', high_col='High', low_col='Lo
         data.at[index, 'ADO'] = ac
 #     data['acc_dist_ema' + str(trend_periods)] = data['acc_dist'].ewm(ignore_na=False, min_periods=0, com=trend_periods, adjust=True).mean()
     return data
-
-
-# In[105]:
-
 
 def gen_macd(data, period_long=26, period_short=12, period_signal=9, column='Close'):
     remove_cols = []
@@ -106,10 +116,6 @@ def gen_macd(data, period_long=26, period_short=12, period_signal=9, column='Clo
     data = data.drop(remove_cols, axis=1)
         
     return data
-
-
-# In[116]:
-
 
 def gen_average_true_range(data, trend_periods=14, open_col='Open', high_col='High', low_col='Low', close_col='Close', drop_tr = True):
     for index, row in data.iterrows():
@@ -130,8 +136,6 @@ def gen_average_true_range(data, trend_periods=14, open_col='Open', high_col='Hi
         
     return data
 
-
-# In[ ]:
 def gen_Momentum(data,trend_periods=9,column='Close'):
     array_list = data["Close"]
     length = len(array_list)
@@ -143,7 +147,6 @@ def gen_Momentum(data,trend_periods=9,column='Close'):
     data["Momentum" + str(trend_periods)]  =  Momen
     return data
 
-# In[ ]:
 def gen_Larry(data,periods_long=14,open_col='Open', high_col='High', low_col='Low', close_col='Close'):
     Close = data[close_col]
     Open = data[open_col]
@@ -192,4 +195,64 @@ def gen_CCI(data, n, high_col='High', low_col='Low', close_col='Close'):
     D = D + 0.001
     CCI = (M - SM)/D/0.015
     data["CCI" + str(n)]  =  CCI
-    return(data)
+    return data
+
+
+# In[88]:
+
+
+df = pd.read_csv(price_filename)
+
+df = gen_simple_ma(df,ma = 5)
+df = gen_ewma(df, ma = 5)
+df = gen_ADO(df, trend_periods=5)
+df = gen_macd(df, period_long=26, period_short=12, period_signal=9)
+df = gen_average_true_range(df, trend_periods=14)
+df = Momentum(df,trend_periods=9)
+df = Larry(df,periods_long=14)
+df = gen_KandD_percent(df, 5)
+df = gen_CCI(df, 5)
+
+
+# In[95]:
+
+
+complete_data = DataFrame()
+month_label = ['9', '6', '3', '12']
+season_label = 0 # point to the according season fundamental data
+year_label = 0
+month_index = -1
+combined_df = pd.DataFrame()
+
+for i in range(len(df)):
+    date = df.loc[i][0].split('/') #list of date
+    year_label = int(date[0])
+    month = int(date[1])
+    if month > 9 and month <= 12:
+        month_index = 3
+        season_label = 3 + 4 * (2019 - year_label)
+    elif month > 0 and month <= 3:
+        month_index = 2
+        season_label = 2 + 4 * (2020 - year_label)
+    elif month > 3 and month <= 6:
+        month_index = 1
+        season_label = 1 + 4 * (2020 - year_label)
+    else:
+        month_index = 0
+        season_label = 4 * (2020 - year_label)
+    tmp_df = pd.DataFrame(df.loc[i].append(data.loc[season_label])).T
+    combined_df = combined_df.append(tmp_df)
+
+
+# In[94]:
+
+
+combined_file_name = price_filename.split('.')[0] + '_combined.csv'
+combined_df.to_csv(combined_file_name, index = False)
+
+
+# In[ ]:
+
+
+
+
